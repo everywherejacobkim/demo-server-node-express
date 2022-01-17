e68510e69b6634e5ef1dd98c3120a536b18769a8
Jacob Namhyung Kim <everywherejacobkim@gmail.com>
2022-01-16 22:23:16 -0800

    feat: create path & request GET

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.listen(); 
+// Open Server
+app.listen(8080, function(){
+    console.log('listening on 8080');
+}); 
+
+app.get('/main', function(req, res){
+    res.send("Hello! You are in Main Page");
+});

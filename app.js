5129930a60c89c9517f08426f2aaa7912932a461
Jacob Namhyung Kim <everywherejacobkim@gmail.com>
2022-01-18 00:59:56 -0800

    feat: practice req.params.type

--- app.js
+++ app.js
@@ -5,7 +5,18 @@ const server = app.listen(process.env.PORT || 8081, ()=>console.log(`Listening o
 
 app.get('/', (req,res)=>{
     res.send(`<h1>Hello Node.js and Express!!!</h1>
-              <h4><i>An introduction server-side scripting</i></h4>`)
+              <h4><i>An introduction to server-side scripting</i></h4>
+              <a href="/about">About</a>`);
 });
 
+app.get('/about', (req, res)=>{
+    res.send('Express is a minimal Node.js framework for making server-side application');
+})
 
+app.get('/animals/:type', (req, res)=>{
+    res.send(`The type of animal you chose is ${req.params.type}`);
+})
+
+app.get('/:something', (req, res)=>{
+    res.send(`You are trying to access ${req.params.something}`);
+})

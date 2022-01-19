dc2387ebd9366983f0db8263e0785ca3d4dadf5a
Jacob Namhyung Kim <everywherejacobkim@gmail.com>
2022-01-18 19:34:49 -0800

    feat: create test pages by nodehandler

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+//*****init express*****//
 const express = require('express');
 const app = express();
-
 const server = app.listen(process.env.PORT || 8081, ()=>console.log(`Listening on port ${process.env.PORT || 8080}`));
+
+
+// express.static => middleware
+app.use(express.static('public'))
 
 app.get('/', (req,res)=>{
     res.send(`<h1>Hello Node.js and Express!!!</h1>
@@ -11,11 +15,11 @@ app.get('/', (req,res)=>{
 
 app.get('/about', (req, res)=>{
     res.send('Express is a minimal Node.js framework for making server-side application');
-})
+});
 
 app.get('/animals/:type', (req, res)=>{
     res.send(`The type of animal you chose is ${req.params.type}`);
-})
+});
 
 // app.get('/:something', (req, res)=>{
 //     res.send(`You are trying to access ${req.params.something}`);
@@ -27,4 +31,20 @@ app.get('/animals/:type', (req, res)=>{
 app.get('/colored-text', (req, res)=>{
     let color = req.query.color;
     res.send(`<p style="color:${color}">The color you chose is ${color}</p>`)
-})
+});
+
+
+//Send whole html file
+app.get("/pages/test", (req, res)=>{
+    res.sendFile(`${__dirname}/test.html`);
+});
+
+
+app.get('/json-test', (req, res)=>{
+    let animal = {
+        type: "dog", 
+        age: 4, 
+        desc: "good dog"
+    }
+    res.json(animal);
+});
